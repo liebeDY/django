957fd6087cfac8162dd86e3ddde9ff363544d366
Dongyun Han <hdydeveloper0927@gmail.com>
2021-06-02 12:35:50 +0900

    장고 2-10 [질문 등록 기능 만들기]

--- mysite/pybo/forms.py
+++ mysite/pybo/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import fields, widgets
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 class QuestionForm(forms.ModelForm) :
 
@@ -8,12 +8,17 @@ class QuestionForm(forms.ModelForm) :
     model = Question
     fields = ['subject', 'content']
 
-    widgets = {
-      'subject' : forms.TextInput(attrs={'class': 'form-control'}),
-      'content' : forms.Textarea(attrs={'class': 'form-control', 'rows': 10})
-    }
-
     labels = {
       'subject': '제목',
       'content': '내용',
+    }
+
+
+
+class AnswerForm(forms.ModelForm) :
+  class Meta :
+    model = Answer
+    fields = ['content']
+    labels = {
+      'content': '답변내용'
     }
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, redirect, render, get_list_or_40
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 def index(request) :
 
@@ -36,8 +36,24 @@ def answer_create(request, question_id) :
   """
 
   question = get_object_or_404(Question, pk=question_id)
-  question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now() )  
-  return redirect('pybo:detail', question_id=question.id)
+
+  if request.method == "POST" :
+    form = AnswerForm(request.POST)
+
+    if form.is_valid() :
+      answer = form.save(commit=False)
+      answer.create_date = timezone.now()
+      answer.question = question
+      answer.save()
+      return redirect('pybo:detail', question_id=question.id)
+
+  else :
+    form = AnswerForm()
+  context = {'question': question, 'form': form}
+
+  return render(request, 'pybo/question_detail.html', context)
+
+
 
 
 def question_create(request) :
