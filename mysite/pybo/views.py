766894948d37e64515e1286e28ef602197ba2ccc
노다혜 <ddhh3@naver.com>
2021-06-04 13:52:47 +0900

    장고 3-07 모델에 글쓴이 추가하기

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -10,6 +10,8 @@ from .forms import QuestionForm, AnswerForm
 
 from django.core.paginator import Paginator
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def index(request):
@@ -35,6 +37,10 @@ def detail(request, question_id):
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context)
 
+
+#answer_create 함수와 question_create 함수는 request.user를 포함하고 있으므로 @login_required 애너테이션을 통해 로그인이 되었는지를 우선 검사하여 앞 단계에서 본 오류를 방지한다. 
+# 만약 로그아웃 상태에서 @login_required 애너테이션이 적용된 함수가 호출되면 자동으로 로그인 화면으로 이동할 것이다. 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     pybo 답변등록
@@ -44,6 +50,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -53,6 +60,7 @@ def answer_create(request, question_id):
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     """
     pybo 질문등록
@@ -61,6 +69,7 @@ def question_create(request):
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
