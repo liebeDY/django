6944f8fce34cbdc006b51785c62e2fbea620b2a0
노다혜 <ddhh3@naver.com>
2021-06-04 14:37:17 +0900

    장고 3-09 게시물 수정&삭제 기능 추가하기

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -5,22 +5,22 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)   #blank=True : form.is_valid()를 통한 입력 폼 데이터 검사 시 값이 없어도 된다는 의미
 
     def __str__(self):
         return self.subject
 
 class Answer(models.Model):
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.content
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,3 +1,4 @@
+from datetime import time
 from django.shortcuts import render, get_object_or_404, redirect
 
 from django.http import HttpResponse
@@ -11,6 +12,8 @@ from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 
 from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
 
 # Create your views here.
 
@@ -77,3 +80,80 @@ def question_create(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """
+    pybo 질문수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')  # messages 모듈 : 오류를 임의로 발생시키고 싶은 경우 (넌필드 오류)
+        return redirect('pybo:detail', question_id=question.id)
+    
+    if request.method == "POST":    # POST 방식으로 호출되어 데이터 수정이 이뤄짐
+        form = QuestionForm(request.POST, instance=question)    # instance 매개변수에 question을 저장하면 기존 값을 폼에 채울 수 있다.
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:   # GET 방식으로 호출되어 질문 수정 화면이 나타남
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    """
+    pybo 질문삭제
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('pybo:detail', question_id = question_id)
+    
+    question.delete()
+    return redirect('pybo:index')
+
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    """
+    pybo 답변수정
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=answer.question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'pybo/answer_form.html', context)
+
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    """
+    pybo 답변삭제
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다')
+    else:
+        answer.delete()
+    return redirect('pybo:detail', question_id=answer.question.id)
