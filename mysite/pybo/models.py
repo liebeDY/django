b0372b58f5450db7670916fe2ff038224fe23355
노다혜 <ddhh3@naver.com>
2021-06-07 15:28:17 +0900

    장고 3-12 추천 기능 추가하기

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -5,22 +5,24 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     modify_date = models.DateTimeField(null=True, blank=True)   #blank=True : form.is_valid()를 통한 입력 폼 데이터 검사 시 값이 없어도 된다는 의미
+    voter = models.ManyToManyField(User, related_name='voter_question')    # voter 추가
 
     def __str__(self):
         return self.subject
 
 class Answer(models.Model):
 
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE, related_name='author_answer')
     content = models.TextField()
     create_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
     def __str__(self):
         return self.content
