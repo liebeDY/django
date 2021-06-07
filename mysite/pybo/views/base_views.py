c666bc322f3d061c91c7ce599abf30b0857b2679
노다혜 <ddhh3@naver.com>
2021-06-07 17:21:49 +0900

    장고 3-15 검색, 정렬 기능 추가하기

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,33 +1,46 @@
 from datetime import time
 from django.shortcuts import render, get_object_or_404, redirect
-
 from django.http import HttpResponse
-
 from ..models import Answer, Question, Comment
-
 from django.utils import timezone
-
 from ..forms import QuestionForm, AnswerForm, CommentForm
-
 from django.core.paginator import Paginator
-
 from django.contrib.auth.decorators import login_required
-
 from django.contrib import messages
+from django.db.models import Q, Count
 
 def index(request):
     """
     pybo 목록 출력
     """
     # 입력 파라미터
-    page = request.GET.get('page', '1')
-    # 조회
-    question_list = Question.objects.order_by('-create_date')
+    page = request.GET.get('page', '1') # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
+    so = request.GET.get('so', 'recent')    #정렬기준
+
+    # 정렬
+    # annotate 함수는 Question 모델의 기존 필드인 author, subject, content, create_date, modify_date, voter에 질문의 추천 수에 해당하는 num_voter 필드를 임시로 추가해 주는 함수이다
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
+    # 검색
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
+
     # 페이징 처리
     paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
