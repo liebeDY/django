fb0648ad71a1c496ce0b1b2c73fb6fa884ffc1db
노다혜 <ddhh3@naver.com>
2021-06-07 16:27:12 +0900

    장고 3-14 마크다운 기능 적용하기

--- mysite/pybo/templatetags/pybo_filter.py
+++ mysite/pybo/templatetags/pybo_filter.py
@@ -1,7 +1,20 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter
 def sub(value, arg):
     return value - arg
+
+
+@register.filter()
+def mark(value):
+    """
+    mark 함수 : markdown 모듈과 mark_safe 함수를 이용하여 문자열을 HTML 코드로 변환하여 반환
+    """
+    extensions = ["nl2br", "fenced_code"]
+    # "nl2br"은 줄바꿈 문자를 <br> 태그로 바꿔 주므로 <Enter>를 한 번만 눌러도 줄바꿈으로 인식
+    # "fenced_code"는 마크다운의 소스 코드 표현을 위해 적용
+    return mark_safe(markdown.markdown(value, extensions=extensions))
